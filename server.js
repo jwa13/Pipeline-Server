4b614bb313b38c1d5e6163884f214a4d2d0e9ab0
Jack Anderson <jack.w.anderson2020@gmail.com>
2025-02-21 17:54:47 -0600

    added route and supporting functions to post new user data to db

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const admin = require("firebase-admin");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -17,6 +18,21 @@ const auth = admin.auth();
 
 app.use(cors());
 app.use(express.json());
+
+const verifyToken = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
+        return res.status(401).json({ message: "Invalid or missing token" });
+    }
+    const token = authHeader.split(" ")[1];
+    try {
+        const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
+        req.decodedToken = decodedToken;
+        next();
+    } catch (error) {
+        return res.status(401).json({message: "Invalid Token"});
+    }
+}
 
 app.get("/api/home", (req, res) => {
     res.json({ message: "Hello World" });
@@ -67,6 +83,59 @@ app.post("/api/login", async (req, res) => {
         const jwtToken = jwt.sign({uid, email}, process.env.JWT_SECRET, {expiresIn: '1h'});
 
         res.json({token: jwtToken});
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+app.post("/api/newAcc", verifyToken, async (req, res) => {
+    try {
+        const userRef = admin.firestore().collection("users").doc(req.decodedToken.uid);
+
+        let updateData = {
+            accType: req.body.type,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            DOB: req.body.dateOfBirth,
+        }
+
+        if (req.body.type == 'athlete') {
+            updateData = {
+                ...updateData,
+                height: req.body.height,
+                weight: req.body.weight,
+                positions: req.body.positions,
+                throwing: req.body.throwing,
+                hitting: req.body.hitting,
+            }
+            if(req.body.phone) {
+                updateData = {
+                    ...updateData,
+                    phone: req.body.phone,
+                }
+            }
+            if(req.body.guardianFirstName) {
+                updateData = {
+                    ...updateData,
+                    guardianFirstName: req.body.guardianFirstName,
+                    guardianLastName: req.body.guardianLastName,
+                    guardianDOB: req.body.guardianDOB,
+                    guardianPhone: req.body.guardianPhone,
+                }
+            }
+        } else if (req.body.type = 'coach') {
+            updateData = {
+                ...updateData,
+                phone: req.body.phone,
+                specialty: req.body.specialty,
+            }
+        } else {
+            return res.status(400).json('Bad form');
+        }
+
+        await userRef.update(updateData);
+        res.status(200);
+
     } catch (error) {
         console.error(error);
     }
